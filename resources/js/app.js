7bdb60e954fefb573a0994c2576d15422a9831c5
Maxime Fondu <maximefondu@icloud.com>
2021-10-03 15:46:39 +0200

    compilation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import ScrollToHash from "./utils/ScrollToHash"
 import Project from "./project"
+// import Asscroll from "./asscroll"
 
 new ScrollToHash()
 new Project()
+// new Asscroll()
---
+++ resources/js/asscroll.js
@@ -0,0 +1,11 @@
+import ASScroll from '@ashthornton/asscroll'
+
+export default class {
+    constructor() {
+        const asscroll = new ASScroll()
+
+        window.addEventListener("load", () => {
+            asscroll.enable()
+        })
+    }
+}
